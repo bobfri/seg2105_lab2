b0347d77d60c03e875f6af2e3ea08099182fe25c
bobfri <36770120+bobfri@users.noreply.github.com>
2021-10-06 10:45:44 -0400

    OnOpenGoogleMaps

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void OnOpenInGoogleMaps(View view){
+        EditText teamAddress = (EditText) findViewById(R.id.teamAddress);
+        Uri gmmIntentUri=Uri.parse("htttp://maps.google.co.in/maps?="+teamAddress.getText());
+        Intent mapIntent= new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
     }
 }
